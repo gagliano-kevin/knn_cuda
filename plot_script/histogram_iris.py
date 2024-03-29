1bac2557d9018e65e495ed1b7ff3a858f5eb96d5
gagliano-kevin <gagliano.kevin.97@gmail.com>
2024-03-29 14:30:26 +0100

    plots

--- plot_script/histogram_iris.py
+++ plot_script/histogram_iris.py
@@ -22,28 +22,27 @@ df['Percentage'] = df.groupby('GPU')['Time'].apply(lambda x: (x / x.sum()) * 100
 
 # Define colors for each operation
 color_map = {
-    'knnDistances (1.96%)': '#4682B4',  # Steel Blue
-    'knn (97.37%)': '#FF6347',           # Tomato
-    'memcpy HtoD (0.30%)': '#32CD32',  # Lime Green
-    'memcpy DtoH (0.08%)': '#4169E1',  # Royal Blue
-    'memset (0.29%)': '#FFAA00',         # Orange
+    'knnDistances (1.96%)': '#4682B4',      # Steel Blue
+    'knn (97.37%)': '#FF6347',              # Tomato
+    'memcpy HtoD (0.30%)': '#32CD32',       # Lime Green
+    'memcpy DtoH (0.08%)': '#4169E1',       # Royal Blue
+    'memset (0.29%)': '#FFAA00',            # Orange
 }
-
 
 # Plot histograms
 fig, axs = plt.subplots(len(total_time_per_gpu), 1, figsize=(10, 6))
 if len(total_time_per_gpu) == 1:
-    axs = [axs]  # Handle single GPU case
+    axs = [axs]                             # Handle single GPU case
 for gpu, ax in zip(total_time_per_gpu.index, axs):
     gpu_data = df[df['GPU'] == gpu]
     ax.bar(gpu_data['Operation'], gpu_data['Percentage'], color=[color_map[op] for op in gpu_data['Operation']])
     ax.set_title(f'GPU {gpu}')
     ax.set_ylabel('Percentage')
-    ax.set_ylim(0, 100)  # Set y-axis limit to percentage scale
-    ax.set_yticks(range(0, 101, 10))  # Set y-axis ticks from 0 to 100 with intervals of 10
+    ax.set_ylim(0, 100)                     # Set y-axis limit to percentage scale
+    ax.set_yticks(range(0, 101, 10))        # Set y-axis ticks from 0 to 100 with intervals of 10
     ax.grid(True, linestyle=':', linewidth=0.5, color='lightgrey')  # Add a light grid
 
-plt.tight_layout()  # Adjust layout to prevent overlap
+plt.tight_layout()                          # Adjust layout to prevent overlap
 
 # Save the plot in a higher resolution and in a different format 
 plt.savefig('histogram_iris.jpg', dpi=300, bbox_inches='tight', format='jpg')
